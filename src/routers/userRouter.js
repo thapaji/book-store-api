5ec4405e0c7eaf0450ac377dbeff6e4ed3a27643
Sujan <mail.thapasujan@gmail.com>
2024-05-14 13:48:15 +1000

    Provision to encrypt password and router endpoint for get and post method created

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,21 +1,42 @@
-import express from 'express'
+import express from "express";
+import { hashPassword } from "../utils/bcrypt.js";
+import { insertUser } from "../model/users/UserModel.js";
 
-const router = express.Router()
+const router = express.Router();
 
-router.all('/', (req, res, next) => {
-    console.log('from all')
-    next();
-})
+router.all("/", (req, res, next) => {
+  console.log("from all");
+  next();
+});
 
-router.get('/', (req, res, next) => {
-    try {
-        res.json({
-            status: 'success',
-            message: 'todo GET'
+router.get("/", (req, res, next) => {
+  try {
+    res.json({
+      status: "success",
+      message: "todo GET",
+    });
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.post("/", async (req, res, next) => {
+  try {
+    req.body.password = hashPassword(req.body.password);
+    const user = await insertUser(req.body);
+
+    user?._id
+      ? res.json({
+          status: "success",
+          message: "Your Account has been created",
         })
-    } catch (error) {
-        next(error)
-    }
-})
+      : es.json({
+          status: "error",
+          message: "Unable to create user. Please try again",
+        });
+  } catch (error) {
+    next(error);
+  }
+});
 
-export default router
+export default router;
