a1c97780ae791456ffc8fa9a086fa430e5dcbb66
Sujan <mail.thapasujan@gmail.com>
2024-06-04 15:18:16 +1000

    saving day work

---
+++ src/model/reviews/ReviewSchema.js
@@ -0,0 +1,42 @@
+import mongoose from 'mongoose'
+
+const BorrowSchema = new mongoose.Schema({
+    userId: {
+        type: mongoose.Types.ObjectId,
+        ref: "User",
+        required: true
+    },
+    userName: {
+        type: String,
+        required: true
+    },
+    bookId: {
+        type: mongoose.Types.ObjectId,
+        ref: "Book",
+        required: true
+    },
+    status: {
+        type: String,
+        default: 'inactive'
+    },
+    title: {
+        type: String,
+        required: true
+    },
+    message: {
+        type: String,
+        required: true
+    },
+    ratings: {
+        type: NUmber,
+        min: 1,
+        max: 5,
+        required: true
+    },
+},
+    {
+        timestamps: true,
+    }
+)
+
+export default mongoose.model('Review', ReviewSchema)
